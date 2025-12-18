717e2d73a080ebadb0ed1b2b34c8687ca2e74398
bala-amypo <bala@amypo.in>
2025-12-18 06:44:14 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,52 +1,63 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import jakarta.persistence.Column;
 @Entity
-
+// @Table(name = "students")
 public class Student {
+
     @Id
-demo/src/main/resources    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id ;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     @NotBlank(message="Name filed cannot be empty")
     @Size(min=3,max=20,message="The user name must be min of 3 and max of 20 character")
-    
     private String name;
+    @Email(message="Invalid email id")
+    @Column(unique=true)
     private String email;
-    
-    
-    public void setId(Long id){
-        this.id=id;
 
-    }
+    // @NotNull
+    // @Size(min,max)
+    // @Min
+    // @Max
+    // @Pattern(reg)
+    // @Postive
+
     public Long getId() {
         return id;
-        
     }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
-    
-    public Student(Long id, String name, String email) {
-        this.id = id;
+
+    public Student(String name,String email) {
         this.name = name;
         this.email = email;
-       
     }
+
     public Student() {
     }
+    
 
-
+    
 }
---
+++ src/main/java/com/example/demo/exception/ResourceNotFoundException.java
@@ -0,0 +1,6 @@
+ package com.example.demo.exception;
+
+ public class ResourseNotFoundException extends RuntimeException{
+    super(message);
+ }
+ }
