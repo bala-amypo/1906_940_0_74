1c40041715935f05f6f5eda0ab02a15c1399d3a0
bala-amypo <bala@amypo.in>
2025-12-18 10:20:27 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -2,10 +2,11 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.http.ResponseEntity;
 import com.example.demo.entity.Student;
 import com.example.demo.service.StudentService;
@@ -13,16 +14,20 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 
 @RestController
+@RequestMapping("/Studentsdata")
 public class StudentController {
     @Autowired
     StudentService studentService;
 
     @PostMapping("/postdata")
     public ResponseEntity<Student> postdata(@Valid @RequestBody Student student){
-        return new ResponseEntity<>(studentService.saveStudent(student),HttpStatus.CREATED);
+        return new  ResponseEntity<>(studentService.saveStudent(student),HttpStatus.CREATED);
     }
+    
     @GetMapping("/getdata/{id}")
     public Student getdata(@PathVariable Long id){
-    return studentService.getStudentById(id);
+         return studentService.getStudentById(id);
     }
+    
+
 }
