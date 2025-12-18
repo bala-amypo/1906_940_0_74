3a027cb2a813bc74975a903b86b5f750890b4c63
bala-amypo <bala@amypo.in>
2025-12-18 10:15:06 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -2,8 +2,10 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.http.ResponseEntity;
 import com.example.demo.entity.Student;
 import com.example.demo.service.StudentService;
@@ -20,5 +22,7 @@ public class StudentController {
         return new ResponseEntity<>(studentService.saveStudent(student),HttpStatus.CREATED);
     }
     @GetMapping("/getdata/{id}")
-    public Student getdata(@PathVariable Long id);
-    return service.getStudentById
+    public Student getdata(@PathVariable Long id){
+    return studentService.getStudentById(id);
+    }
+}
